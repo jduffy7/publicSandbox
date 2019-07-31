8ce66d80d0eb4c272a1e919ad08c1c6fe8d2eea5
jduffy7 <jduffy7@gmail.com>
2019-07-31 14:38:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const moment = require('./node_modules/moment');
+const { firstBy } = require('./node_modules/thenby');
+const {nerdvision} = require('./node_modules/@nerdvision/agent');
+
+nerdvision.init('')
+    .then(() => console.log('lets go!'))
+    .then(() => {
+        let startTime = '2019-09-11T23:30:00-06:00';
+        let m = moment().toISOString(true);
+        let n = moment().toISOString(false);
+        console.log(m);
+        console.log(n);
+        // remember to close the connection when your application exits
+        nerdvision.close();
+    })
+
