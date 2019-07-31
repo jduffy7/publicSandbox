e2cefba91520d2b8c0a2d03e820b8e5a02c3e2a3
jduffy7 <jduffy7@gmail.com>
2019-07-31 14:45:37 -0400

    2!

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const moment = require('./node_modules/moment');
 const { firstBy } = require('./node_modules/thenby');
 const {nerdvision} = require('./node_modules/@nerdvision/agent');
 
-nerdvision.init(process.argv[1])
-    .then(() => console.log('lets go!'))
+nerdvision.init(process.argv[2])
+    .then(() => console.log('lets'))
     .then(() => {
         let startTime = '2019-09-11T23:30:00-06:00';
         let m = moment().toISOString(true);
